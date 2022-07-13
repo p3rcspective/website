3c9c9cf675649bcb3fafff541bcb72cfaacd10e3
p3rcspective <109171364+p3rcspective@users.noreply.github.com>
2022-07-14 00:23:55 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,11 +1,3 @@
-/*
-Most of the stuff in here is just bootstrapping. Essentially it's just
-setting ThreeJS up so that it renders a flat surface upon which to draw 
-the shader. The only thing to see here really is the uniforms sent to 
-the shader. Apart from that all of the magic happens in the HTML view
-under the fragment shader.
-*/
-
 let container;
 let camera, scene, renderer;
 let uniforms;
