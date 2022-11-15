61c95ce2f2183d915bb7c5a893c22c0c9a807e71
p3rcspective <109171364+p3rcspective@users.noreply.github.com>
2022-11-15 17:35:51 +0000

    Add files via upload

---
+++ src/scripts/p3rcspective.js
@@ -0,0 +1,15 @@
+
+var string = "p3rcspective!"; /* text */
+var array = string.split("");
+var timer;
+
+function frameLooper () {
+	if (array.length > 0) {
+		document.getElementById("p3rcspective").innerHTML += array.shift();
+	} else {
+		clearTimeout(timer);
+			}
+	loopTimer = setTimeout('frameLooper()',500); /* speed */
+
+}
+frameLooper();
