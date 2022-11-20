2a7c41e1519e1f51453218e383f7fda58fb4b056
p3rcspective <109171364+p3rcspective@users.noreply.github.com>
2022-11-20 15:56:18 +0000

    Delete src directory

--- src/scripts/fade.js
+++
@@ -1,9 +0,0 @@
-$(document).ready(function() {
-    $(window).scroll(function(event) {
-        let scroll = $(this).scrollTop();
-        let opacity = 1 - (scroll / 1000);
-        if (opacity >= 0) {
-            $('.text-center-about').css('0', opacity);
-        }
-    });
-});
